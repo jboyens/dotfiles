b1fef809f2af3a0f756ed7a8482ecaf69952ad4e
JR Boyens <jboyens@fooninja.org>
2020-12-11 15:24:47 -0800

    Set up a systemd user service for offlineimap

--- modules/email/mu4e.nix
+++ modules/email/mu4e.nix
@@ -6,13 +6,41 @@ let cfg = config.modules.email.mu4e;
 in {
   options.modules.email.mu4e = {
     enable = mkBoolOpt false;
+    package = mkOption  {
+      type = types.package;
+      default = pkgs.offlineimap;
+      defaultText = "pkgs.offlineimap";
+      description = "Offlineimap derivation to use";
+    };
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      offlineimap
+      cfg.package
       mu
       imapfilter
     ];
+
+    systemd.user.services.offlineimap = {
+      enable = true;
+      description = "Offlineimap: a software to dispose your mailbox(es) as a local Maildir(s)";
+      serviceConfig = {
+        Type      = "oneshot";
+        ExecStart = "${cfg.package}/bin/offlineimap -u syslog -o";
+        TimeoutStartSec = "240sec";
+      };
+      path = with pkgs; [ imapfilter ];
+    };
+    systemd.user.timers.offlineimap = {
+      enable = true;
+      description = "offlineimap timer";
+      timerConfig               = {
+        Unit = "offlineimap.service";
+        OnCalendar = "*:0/15";
+        # start immediately after computer is started:
+        Persistent = "true";
+      };
+      wantedBy = [ "default.target" ];
+    };
   };
 }
