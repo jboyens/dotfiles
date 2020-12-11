9e6fc57cd329adc9480101882d1b449c33a20974
JR Boyens <jboyens@fooninja.org>
2020-12-11 15:24:14 -0800

    Add a private overlay

---
+++ overlays/jboyens.nix
@@ -0,0 +1,11 @@
+self: super:
+{
+  offlineimap = super.offlineimap.overrideAttrs (old: {
+    src = super.fetchFromGitHub {
+      owner = "OfflineIMAP";
+      repo = "offlineimap";
+      rev = "2d0d07cd6a0560e5845ac09a0b3fbada3a034ba6";
+      sha256 = "NU/kqsBUPR+0EnEDIXMQaBU6gm2Y+KExH5XWKMFJ2x0=";
+    };
+  });
+}
