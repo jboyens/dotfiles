28e42fa2f5ebd0e45cdf2e8e7cd1bcaa20e40f1e
JR Boyens <jboyens@fooninja.org>
2020-12-18 12:03:43 -0800

    Don't install this stuff unless modules.dev.cloud.enable = true

--- modules/dev/cloud.nix
+++ modules/dev/cloud.nix
@@ -7,6 +7,7 @@ with lib.my;
 let cfg = config.modules.dev.cloud;
 in {
   options.modules.dev.cloud = {
+    enable = mkBoolOpt false;
     google.enable = mkBoolOpt false;
     amazon.enable = mkBoolOpt false;
     microsoft.enable = mkBoolOpt false;
@@ -17,15 +18,11 @@ in {
       user.packages = with pkgs; [ google-cloud-sdk cloud-sql-proxy ];
     })
 
-    (mkIf cfg.amazon.enable {
-      user.packages = with pkgs; [ awscli ];
-    })
+    (mkIf cfg.amazon.enable { user.packages = with pkgs; [ awscli ]; })
 
-    (mkIf cfg.microsoft.enable {
-      user.packages = with pkgs; [ azure-cli ];
-    })
+    (mkIf cfg.microsoft.enable { user.packages = with pkgs; [ azure-cli ]; })
 
-    ({
+    (mkIf cfg.enable {
       user = {
         packages = with pkgs; [
           unstable.terraform
@@ -35,6 +32,7 @@ in {
           unstable.kubernetes-helm
           my.krew
           unstable.open-policy-agent
+          my.istioctl_1_5
         ];
       };
 
