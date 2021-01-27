d23cac352515b6bbb7447dbcc009d22d2f17ffc7
JR Boyens <jboyens@fooninja.org>
2021-01-27 05:32:32 -0800

    Basic wireguard setup

--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -16,7 +16,19 @@ in {
       networking.nat.enable = true;
       networking.nat.externalInterface = "enp0s20u1";
       networking.nat.internalInterfaces = [ "wg0" ];
-      networking.firewall = { allowedUDPPorts = [ 51820 ]; };
+      networking.firewall = {
+        allowedTCPPorts = [ 53 ];
+        allowedUDPPorts = [ 53 51820 ];
+      };
+
+      services.dnsmasq = {
+        enable = true;
+        servers = [ "8.8.8.8" ];
+        extraConfig = ''
+          interface=wg0
+          log-queries=extra
+        '';
+      };
 
       networking.wireguard.interfaces = {
         wg0 = {
@@ -26,20 +38,28 @@ in {
           # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
           # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
           postSetup = ''
-            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+            ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp0s20u1 -j MASQUERADE
           '';
 
           # This undoes the above command
           postShutdown = ''
-            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+            ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp0s20u1 -j MASQUERADE
           '';
 
           privateKeyFile =
             "/home/jboyens/.secrets/wireguard/irongiant/private.key";
-          peers = [{
-            publicKey = "jGoS2hb7yLIg9OEhl7xSCid790yiREjO4lk36m2pVAA=";
-            allowedIPs = [ "10.100.0.2/32" ];
-          }];
+          peers = [
+            {
+              publicKey = "jGoS2hb7yLIg9OEhl7xSCid790yiREjO4lk36m2pVAA=";
+              allowedIPs = [ "10.100.0.2/32" ];
+            }
+            {
+              publicKey = "cAnOtDzfBUL+c8Nu5Vc666JxcDfXkm/rIjo0Dh99LiA=";
+              allowedIPs = [ "10.100.0.3/32" ];
+            }
+          ];
         };
       };
     })
@@ -48,8 +68,7 @@ in {
         wg0 = {
           ips = [ "10.100.0.2/24" ];
 
-          privateKeyFile =
-            "/home/jboyens/.secrets/wireguard/kitt/private.key";
+          privateKeyFile = "/home/jboyens/.secrets/wireguard/kitt/private.key";
           peers = [{
             publicKey = "kxhD+BPEcgdK2N7KfZ/05QOhYtTMR0r8CccuYRpx8mc=";
             allowedIPs = [ "192.168.86.0/24" "10.100.0.0/24" ];
