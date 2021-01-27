34b5f00aefa75001d449176a2c27eed212a9adb1
JR Boyens <jboyens@fooninja.org>
2021-01-27 05:35:03 -0800

    Let's see if pinentryFlavor does the right thing
    This only works with hosts that have an X or Wayland display otherwise

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -5,8 +5,8 @@ with lib.my;
 let cfg = config.modules.shell.gnupg;
 in {
   options.modules.shell.gnupg = with types; {
-    enable   = mkBoolOpt false;
-    cacheTTL = mkOpt int 3600;  # 1hr
+    enable = mkBoolOpt false;
+    cacheTTL = mkOpt int 3600; # 1hr
   };
 
   config = mkIf cfg.enable {
@@ -19,10 +19,10 @@ in {
     # HACK Without this config file you get "No pinentry program" on 20.03.
     #      programs.gnupg.agent.pinentryFlavor doesn't appear to work, and this
     #      is cleaner than overriding the systemd unit.
+    # pinentry-program ${pkgs.pinentry.curses}/bin/pinentry
     home.configFile."gnupg/gpg-agent.conf" = {
       text = ''
         default-cache-ttl ${toString cfg.cacheTTL}
-        pinentry-program ${pkgs.pinentry.gtk2}/bin/pinentry
       '';
     };
   };
