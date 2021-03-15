73df3d6b420bbfea9b8be521c3213e2efec9127c
JR Boyens <jboyens@fooninja.org>
2021-03-15 00:17:43 -0700

    Add just enough hackery to get some X11 utils
    This is intended to get emacs-everywhere to work. It's close, but I've
    got a bug in wl-clipboard-x11 that fails to pull the final file into the clipboard

---
+++ packages/bitwarden-gui.nix
@@ -0,0 +1,23 @@
+{ stdenv, fetchurl, appimageTools }:
+
+let
+  pname = "bitwarden";
+  version = "1.25.0";
+in appimageTools.wrapType2 rec {
+  name = "${pname}-${version}";
+
+  src = fetchurl {
+    url = "https://github.com/bitwarden/desktop/releases/download/v1.25.0/Bitwarden-1.25.0-x86_64.AppImage";
+    sha256 = "1549eb9136393c9593d84f782874604fa4af7fc344d484a5483bcda186de5a8d";
+  };
+
+  extraPkgs = pkgs: with pkgs; [ libsecret ];
+
+  meta = with stdenv.lib; {
+    description = "A wrapper to use wl-clipboard as a drop-in replacement to X11 clipboard tools";
+    homepage    = "https://github.com/brunelli/wl-clipboard-x11";
+    license     = licenses.gpl3;
+    platforms   = platforms.linux;
+    maintainers = with maintainers; [ jboyens ];
+  };
+}
