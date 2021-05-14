1dff0614dcd289f2ede0a4e886dca9a620f1a379
JR Boyens <jboyens@fooninja.org>
2021-05-13 18:03:06 -0700

    Remove the offlineimap overlay

--- overlays/jboyens.nix
+++ overlays/jboyens.nix
@@ -1,11 +1,3 @@
 final: prev:
 {
-  offlineimap = prev.offlineimap.overrideAttrs (old: {
-    src = prev.fetchFromGitHub {
-      owner = "OfflineIMAP";
-      repo = "offlineimap";
-      rev = "2d0d07cd6a0560e5845ac09a0b3fbada3a034ba6";
-      sha256 = "NU/kqsBUPR+0EnEDIXMQaBU6gm2Y+KExH5XWKMFJ2x0=";
-    };
-  });
 }
