3c53fcd40f4addfbae61a3a58dc9827f50fe210d
JR Boyens <jboyens@fooninja.org>
2022-09-08 21:43:56 -0700

    Bump

--- modules/desktop/swaywm.nix
+++ modules/desktop/swaywm.nix
@@ -34,8 +34,8 @@ in {
         # Fix for some Java AWT applications (e.g. Android Studio),
         # use this if they aren't displayed properly:
         export _JAVA_AWT_WM_NONREPARENTING=1
-        # export MOZ_WEBRENDER=1
-        # export MOZ_ENABLE_WAYLAND=1
+        export MOZ_WEBRENDER=1
+        export MOZ_ENABLE_WAYLAND=1
         export MOZ_DBUS_REMOTE=1
         export XDG_SESSION_TYPE=wayland
         export XDG_CURRENT_DESKTOP=sway
