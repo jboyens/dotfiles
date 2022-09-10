4f1d7504fedf88e9a18d7cdecd8e0d6dba412e66
JR Boyens <jboyens@fooninja.org>
2022-09-10 01:54:54 -0700

    Setup GTK tweaks again...

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -12,34 +12,54 @@ in {
     active = mkOption {
       type = nullOr str;
       default = null;
-      apply = v: let theme = builtins.getEnv "THEME"; in
-                 if theme != "" then theme else v;
+      apply = v:
+        let theme = builtins.getEnv "THEME";
+        in if theme != "" then theme else v;
       description = ''
         Name of the theme to enable. Can be overridden by the THEME environment
         variable. Themes can also be hot-swapped with 'hey theme $THEME'.
       '';
     };
 
-    onReload = mkOpt (attrsOf lines) {};
+    onReload = mkOpt (attrsOf lines) { };
   };
 
   config = mkIf (cfg.active != null) (mkMerge [
-    (mkIf (cfg.onReload != {})
-      (let reloadTheme =
-             with pkgs; (writeScriptBin "reloadTheme" ''
-               #!${stdenv.shell}
-               echo "Reloading current theme: ${cfg.active}"
-               ${concatStringsSep "\n"
-                 (mapAttrsToList (name: script: ''
-                   echo "[${name}]"
-                   ${script}
-                 '') cfg.onReload)}
-             '');
-       in {
-         user.packages = [ reloadTheme ];
-         system.userActivationScripts.reloadTheme = ''
-           [ -z "$NORELOAD" ] && ${reloadTheme}/bin/reloadTheme
-         '';
-       }))
+    {
+      home-manager.users.${config.user.name}.gtk = {
+        gtk3 = {
+          extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+            gtk-xft-hinting = 1;
+            gtk-xft-hintstyle = "hintfull";
+            gtk-xft-rgba = "none";
+          };
+        };
+        gtk4 = {
+          extraConfig = {
+            gtk-application-prefer-dark-theme = true;
+            gtk-xft-hinting = 1;
+            gtk-xft-hintstyle = "hintfull";
+            gtk-xft-rgba = "none";
+          };
+        };
+      };
+    }
+    (mkIf (cfg.onReload != { }) (let
+      reloadTheme = with pkgs;
+        (writeScriptBin "reloadTheme" ''
+          #!${stdenv.shell}
+          echo "Reloading current theme: ${cfg.active}"
+          ${concatStringsSep "\n" (mapAttrsToList (name: script: ''
+            echo "[${name}]"
+            ${script}
+          '') cfg.onReload)}
+        '');
+    in {
+      user.packages = [ reloadTheme ];
+      system.userActivationScripts.reloadTheme = ''
+        [ -z "$NORELOAD" ] && ${reloadTheme}/bin/reloadTheme
+      '';
+    }))
   ]);
 }
