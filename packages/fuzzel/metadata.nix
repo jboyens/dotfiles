c479d692e8440e2aa84b726b1aff27ab1041ad19
JR Boyens <jboyens@fooninja.org>
2023-09-09 13:58:55 -0700

    Get rid of a bunch of old trash

--- packages/fuzzel/metadata.nix
+++
@@ -1,9 +0,0 @@
-rec {
-  domain = "git.sr.ht";
-  owner = "~dnkl";
-  repo = "fuzzel";
-  repo_git = "https://${domain}/${owner}/${repo}";
-  branch = "master";
-  rev = "d904bc05323cd2977c493531219ad6e4aefe45b5";
-  sha256 = "sha256-2lTo3FfmREFgyoTF2p9Au9FzCSmxU6LOKfSHjV/3pZM=";
-}
