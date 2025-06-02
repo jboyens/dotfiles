fb94356c64eeef1e5d8190cd2daf1c91087517ae
JR Boyens <jboyens@moment.dev>
2025-06-02 09:43:46 -0700

    bump

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -7,45 +7,45 @@
   inherit (pkgs) system;
   inherit (self.packages."${system}") moment-staging;
 in {
-  home.packages = with pkgs; [
-    widevine-cdm
+  home = {
+    packages = with pkgs; [
+      widevine-cdm
 
-    ghostty
+      ghostty
 
-    gnomeExtensions.tiling-shell
+      gnomeExtensions.tiling-shell
 
-    moment-staging
+      moment-staging
 
-    flatpak
-    gnome-software
-  ];
+      flatpak
+      gnome-software
+    ];
 
-  home.sessionVariables = let
-    XDG_DATA_HOME = config.home.sessionVariables.XDG_DATA_HOME;
-    XDG_CONFIG_HOME = config.home.sessionVariables.XDG_CONFIG_HOME;
-    XDG_CACHE_HOME = config.home.sessionVariables.XDG_CACHE_HOME;
-  in {
-    ANDROID_USER_HOME = "${XDG_DATA_HOME}/android";
-    AWS_SHARED_CREDENTIALS_FILE = "${XDG_CONFIG_HOME}/aws/credentials";
-    AWS_CONFIG_FILE = "${XDG_CONFIG_HOME}/aws/config";
-    CARGO_HOME = "${XDG_DATA_HOME}/cargo";
-    GRADLE_USER_HOME = "${XDG_DATA_HOME}/gradle";
-    IMAPFILTER_HOME = "${XDG_CONFIG_HOME}/imapfilter";
-    NODE_REPL_HISTORY = "${XDG_DATA_HOME}/node_repl_history";
-    PKG_CACHE_PATH = "${XDG_DATA_HOME}/pkg-cache";
-    PGPASSFILE = "${XDG_CONFIG_HOME}/pg/pgpass";
-    PSQL_HISTORY = "${XDG_DATA_HOME}/psql_history";
-    BUNDLE_USER_CONFIG = "${XDG_CONFIG_HOME}/bundle";
-    BUNDLE_USER_CACHE = "${XDG_CACHE_HOME}/bundle";
-    BUNDLE_USER_PLUGIN = "${XDG_DATA_HOME}/bundle";
-    RUSTUP_HOME = "${XDG_DATA_HOME}/rustup";
-    W3M_DIR = "${XDG_DATA_HOME}/w3m";
-    WINEPREFIX = "${XDG_DATA_HOME}/wine";
-  };
+    sessionVariables = let
+      inherit (config.home.sessionVariables) XDG_DATA_HOME XDG_CONFIG_HOME XDG_CACHE_HOME;
+    in {
+      ANDROID_USER_HOME = "${XDG_DATA_HOME}/android";
+      AWS_SHARED_CREDENTIALS_FILE = "${XDG_CONFIG_HOME}/aws/credentials";
+      AWS_CONFIG_FILE = "${XDG_CONFIG_HOME}/aws/config";
+      CARGO_HOME = "${XDG_DATA_HOME}/cargo";
+      GRADLE_USER_HOME = "${XDG_DATA_HOME}/gradle";
+      IMAPFILTER_HOME = "${XDG_CONFIG_HOME}/imapfilter";
+      NODE_REPL_HISTORY = "${XDG_DATA_HOME}/node_repl_history";
+      PKG_CACHE_PATH = "${XDG_DATA_HOME}/pkg-cache";
+      PGPASSFILE = "${XDG_CONFIG_HOME}/pg/pgpass";
+      PSQL_HISTORY = "${XDG_DATA_HOME}/psql_history";
+      BUNDLE_USER_CONFIG = "${XDG_CONFIG_HOME}/bundle";
+      BUNDLE_USER_CACHE = "${XDG_CACHE_HOME}/bundle";
+      BUNDLE_USER_PLUGIN = "${XDG_DATA_HOME}/bundle";
+      RUSTUP_HOME = "${XDG_DATA_HOME}/rustup";
+      W3M_DIR = "${XDG_DATA_HOME}/w3m";
+      WINEPREFIX = "${XDG_DATA_HOME}/wine";
+    };
 
-  home.shellAliases = {
-    adb = ''HOME="$XDG_DATA_HOME"/android adb'';
-    wget = ''wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'';
+    shellAliases = {
+      adb = ''HOME="$XDG_DATA_HOME"/android adb'';
+      wget = ''wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'';
+    };
   };
 
   xdg.mimeApps = {
@@ -60,14 +60,5 @@ in {
     };
   };
 
-  dconf = {
-    enable = true;
-
-    settings."org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = with pkgs.gnomeExtensions; [
-        tiling-shell.extensionUuid
-      ];
-    };
-  };
+  dconf.enable = true;
 }
