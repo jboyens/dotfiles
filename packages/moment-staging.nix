bec1845ca3dd6c02fabd5bc8bd641ce5e8d6f9f7
JR Boyens <jboyens@moment.dev>
2025-08-08 20:53:05 -0700

    Update moment

--- packages/moment-staging.nix
+++ packages/moment-staging.nix
@@ -16,11 +16,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "moment-staging";
-  version = "0.3.572-staging+791";
+  version = "0.3.591-staging+951";
 
   src = fetchurl {
     url = "https://d1zyf2h5975v9k.cloudfront.net/tauri/linux/%5Bstaging%5D+Moment_${lib.escapeURL version}_amd64.deb";
-    sha256 = "sha256-GU8YtFp6COv2XO1PJGvGjYg96mtHbG+I6iUXHbTDVgc=";
+    sha256 = "sha256-268PA3rSI3da6Ua6zoyHIF7k/g5o8oJDu6ivitJGmDM=";
   };
 
   nativeBuildInputs = [
