2f963a610f60b7fe5cd7d9e58d4c92f53d2a67f6
JR Boyens <jboyens@fooninja.org>
2026-03-02 18:25:19 -0800

    change: tinman

--- nixos-configurations/tinman/default.nix
+++ nixos-configurations/tinman/default.nix
@@ -1,11 +1,11 @@
 {
   inputs,
-  lib,
   pkgs,
+  ezModules,
   ...
 }: let
+  inherit (pkgs) lib;
   kernel = pkgs.linuxPackages_latest;
-  lib = pkgs.lib // builtins;
 
   defaults = {
     hardware.enableRedistributableFirmware = true;
@@ -14,8 +14,9 @@
 in {
   imports =
     [
-      # inputs.lix-module.nixosModules.default
       inputs.home-manager.nixosModules.default
+      ezModules.vaultwarden
+      ezModules.blocky
     ]
     ++ (with inputs.nixos-hardware.nixosModules; [
       defaults
@@ -28,7 +29,7 @@ in {
   nixpkgs.hostPlatform = "x86_64-linux";
 
   time.timeZone = "America/Los_Angeles";
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.11";
 
   home-manager = {
     useUserPackages = true;
@@ -37,6 +38,7 @@ in {
 
   networking = {
     hostName = "tinman";
+    firewall.allowedTCPPorts = [5055 9000];
   };
 
   security.sudo.wheelNeedsPassword = false;
@@ -71,7 +73,6 @@ in {
     '';
   };
 
-  # bluetooth
   hardware.bluetooth = {
     enable = true;
     settings = {
@@ -82,15 +83,88 @@ in {
   services = {
     thermald.enable = true;
 
-    # thunderbolt
     hardware.bolt.enable = true;
 
-    # firmware updates
     fwupd = {
       enable = true;
-      extraRemotes = [
-        "lvfs"
-        # "dell-esrt"
+      extraRemotes = ["lvfs"];
+    };
+
+    promtail = {
+      enable = true;
+      configuration = {
+        clients = [
+          {url = "http://192.168.86.100:3100/loki/api/v1/push";}
+        ];
+
+        scrape_configs = [
+          {
+            job_name = "system";
+            static_configs = [
+              {
+                targets = ["localhost"];
+                labels = {
+                  job = "varlogs";
+                  host = "192.168.86.246";
+                  __path__ = "/var/log/*.log";
+                };
+              }
+            ];
+          }
+          {
+            job_name = "journal";
+            journal = {
+              labels = {
+                job = "systemd-journal";
+                host = "192.168.86.246";
+              };
+              max_age = "12h";
+            };
+            relabel_configs = [
+              {
+                source_labels = ["__journal__systemd_unit"];
+                target_label = "unit";
+              }
+            ];
+          }
+        ];
+
+        server = {
+          http_listen_port = 9000;
+          grpc_listen_port = 0;
+        };
+      };
+    };
+  };
+
+  virtualisation.oci-containers.containers = {
+    overseerr = {
+      image = "sctx/overseerr";
+      environment = {
+        TZ = "America/Los_Angeles";
+        PUID = "1000";
+        PGID = "1000";
+        LOG_LEVEL = "debug";
+      };
+      volumes = [
+        "/home/jboyens/config/overseerr:/app/config"
+      ];
+      ports = [
+        "5055:5055/tcp"
+      ];
+    };
+
+    watchtower = {
+      image = "ghcr.io/containrrr/watchtower";
+      volumes = [
+        "/var/run/docker.sock:/var/run/docker.sock"
+      ];
+      environment = {
+        WATCHTOWER_CLEANUP = "true";
+        WATCHTOWER_POLL_INTERVAL = "86400";
+      };
+      extraOptions = [
+        "--privileged"
       ];
     };
   };
